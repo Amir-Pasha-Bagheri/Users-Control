4a6ecc4bec290da2e3e2343993984100fc1f9395
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-04-11 18:01:28 +0430

    HomaPage Changes

--- client/src/pages/Homepage/homepage.js
+++ client/src/pages/Homepage/homepage.js
@@ -1,5 +1,7 @@
 import React ,{Component}from 'react'
 import axios from 'axios'
+import './homepage.css'
+import {Link} from 'react-router-dom'
 
 export default class Homepage extends Component {
     state = {data:''}
@@ -12,7 +14,8 @@ export default class Homepage extends Component {
     render(){
         return(
             <React.Fragment>
-                {this.state.data}
+                <Link to="/Signin"><button className='button'>Sign In</button></Link>
+                <Link to="/Login"><button className='button'>Log In</button></Link>
             </React.Fragment>
         )
     }
