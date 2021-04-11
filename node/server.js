ba3732ddc0ac2bbc72fd84de0556dc13aaac5909
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-04-11 18:33:46 +0430

    Add SignIn And LogIn API

--- node/server.js
+++ node/server.js
@@ -8,4 +8,12 @@ app.get('/',(req,res)=>{
     res.send('home')
 })
 
+app.get('/Signin',(req,res)=>{
+    res.send('Sign In Page')
+})
+
+app.get('/Login',(req,res)=>{
+    res.send('Log In Page')
+})
+
 app.listen(8080,()=>{console.log('Running On Port 8000...')})
