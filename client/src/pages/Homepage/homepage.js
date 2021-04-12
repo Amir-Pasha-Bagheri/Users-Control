e4016919152380484395863c2601d75c44c3fd9b
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-04-12 19:12:18 +0430

    Passing Data For Creating Account

--- client/src/pages/Homepage/homepage.js
+++ client/src/pages/Homepage/homepage.js
@@ -4,11 +4,12 @@ import './homepage.css'
 import {Link} from 'react-router-dom'
 
 export default class Homepage extends Component {
-    state = {data:''}
+    state = {currentUser:null}
 
+    
     componentDidMount(){
         axios.get('http://localhost:8080/')
-        .then(res=>this.setState({data:res.data}))
+        .then(res=>this.setState({currentUser:res.data.username}))
     }
 
     render(){
@@ -16,7 +17,8 @@ export default class Homepage extends Component {
             <React.Fragment>
                 <h3>{this.state.data}</h3>
                 <Link to="/Signin"><button className='button'>Sign In</button></Link>
-                <Link to="/Login"><button className='button'>Log In</button></Link>
+                <Link to="/Login"><button className='button'>Log In</button></Link><br/>
+                {this.state.currentUser!==null?<Link to='/Login'><button className='buttonAccount'>{this.state.currentUser}</button></Link>:false}
             </React.Fragment>
         )
     }
--- node/server.js
+++ node/server.js
@@ -2,14 +2,36 @@ const express = require('express')
 const cors =  require('cors')
 
 const app = express()
+
+const users = []
+let currentUser = {
+    username:null,
+    password:null
+}
+
+app.use(express.json())
 app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.send('home')
+    res.send(currentUser)
 })
 
 app.get('/Signin',(req,res)=>{
     res.send('Sign In Page')
+})
+
+app.post('/Signin',(req,res)=>{
+    const checkUser = users.find(user=>user.username===req.body.username)
+    if(checkUser){
+        res.send('This Username Already Exist !')
+    }
+    else{
+        const newUser = {username:req.body.username,password:req.body.password}
+        currentUser = newUser
+        users.push(newUser)
+        res.send('ok')
+        console.log(users);
+    }
 })
 
 app.get('/Login',(req,res)=>{
