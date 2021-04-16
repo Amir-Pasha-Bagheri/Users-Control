efcc7f9e5152af0ef15604e158bc967502a03a3c
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-04-16 18:46:54 +0430

    Adding Passport.js To Project

--- node/server.js
+++ node/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const cors =  require('cors')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
 
 const app = express()
+
+const initializePassport = require('./passport-config')
+initializePassport(passport,username=>users.find(user=>user.username===username))
 
 const users = []
 let currentUser = {
@@ -11,27 +16,23 @@ let currentUser = {
 
 app.use(express.json())
 app.use(cors())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/',(req,res)=>{
     res.send(currentUser)
 })
 
 app.get('/Signin',(req,res)=>{
-    res.send('Sign In Page')
+    res.send('Sign Up Page')
 })
 
-app.post('/Signin',(req,res)=>{
-    const checkUser = users.find(user=>user.username===req.body.username)
-    if(checkUser){
-        res.send('This Username Already Exist !')
-    }
-    else{
-        const newUser = {username:req.body.username,password:req.body.password}
-        users.push(newUser)
-        res.send('ok')
-        currentUser.username = req.body.username
-        currentUser.password = req.body.password
-    }
+app.post('/Signin',async (req,res)=>{
+    const hashedPassword = bcrypt.hash(req.body.password, 10)
+    users.push({
+        id: Date.now().toString(),
+        username: req.body.username ,
+        password: hashedPassword
+    })
 })
 
 app.get('/Login',(req,res)=>{
